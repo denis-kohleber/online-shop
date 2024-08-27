4fa573d14e8cfc91c4fd6fc53e60842c79105bfc
Denis-K0 <160408077+Denis-K0@users.noreply.github.com>
2024-08-27 20:12:19 +0200

    Neuaufsätzen der Navbar (Design, Accessibility)

--- veritas-shop/src/components/Header.tsx
+++ veritas-shop/src/components/Header.tsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom"
 const Header = forwardRef<HTMLElement>((props, ref) => {
     return (
       <header className="header" ref={ref}>
-        <Link to="/">
-          <img className="logo" src="src/assets/veritas-logo.svg" alt="veritas" />
+        <div className="headerBg"></div>
+        <Link to="/" aria-hidden="true" tabIndex={-1}>
+          <img className="logo" src="src/assets/veritas-logo.svg" alt="veritas"/>
         </Link>
       </header>
     )
--- veritas-shop/src/components/Navbar.tsx
+++ veritas-shop/src/components/Navbar.tsx
@@ -1,26 +1,82 @@
 import { useEffect, useRef, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { useWindowWidth } from '../hooks/useWindowWidth';
 
 interface Props {
     inView: boolean;
 }
 
 function Navbar({ inView }: Props) {
-  // State for the menus
+  // States for the menus
   const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
   const [isCartActive, setIsCartActive] = useState<boolean>(false);
   const [isSearchActive, setIsSearchActive] = useState<boolean>(false);
 
-  const showMenu = () => {
+  const burgerBtn = useRef<HTMLButtonElement>(null);
+  const navLink01 = useRef<HTMLButtonElement>(null);
+  const navLink02 = useRef<HTMLAnchorElement>(null);
+  const navLink03 = useRef<HTMLAnchorElement>(null);
+  const navLink04 = useRef<HTMLAnchorElement>(null);
+  const navLink05 = useRef<HTMLAnchorElement>(null);
+  const navLinks = [navLink01, navLink02, navLink03, navLink04, navLink05];
+
+  // Watch screen-width on resize
+  const windowWidth = useWindowWidth();
+
+  // Change Tabindex by < 800px screen-width
+  useEffect(() => {
+    if (windowWidth < 800) {
+      navLinks.forEach(element => {
+        if (element.current) element.current.tabIndex = -1;
+      });
+    } else {
+      navLinks.forEach(element => {
+        if (element.current) element.current.tabIndex = 0;
+    })
+  }}, [windowWidth])
+
+  const activateTabIndexMenu = () => {
+    navLinks.forEach(element => {
+      if (element.current) element.current.tabIndex = 0;
+    })
+  };
+
+  const deactivateTabIndexMenu = () => {
+    navLinks.forEach(element => {
+      if (element.current) element.current.tabIndex = -1;
+    });
+  } 
+
+  // Function that hold the tab-navigation in the open menu
+  const handleBlurLastLink = 
+  (e: React.KeyboardEvent<HTMLAnchorElement>) => {
+    if (windowWidth > 800) return;
+
+    if (e.key === 'Tab' && !e.shiftKey) {
+        e.preventDefault();
+        if (burgerBtn.current) burgerBtn.current.focus();
+    }
+  }
+
+  const onClickMenuLink = () => {
+    if (windowWidth > 800) return;
+    showCloseMenu();
+  }
+
+  const showCloseMenu = () => {
     jumpToNav();
     setIsCartActive(() => false);
     setIsSearchActive(() => false);
+
+    if (isMenuActive) deactivateTabIndexMenu();
+    if (!isMenuActive) activateTabIndexMenu();
     setIsMenuActive(prevState => !prevState);
   };
 
   const showCart = () => {
     jumpToNav();
     setIsMenuActive(() => false);
+    deactivateTabIndexMenu();
     setIsSearchActive(() => false);
     setIsCartActive(prevState => !prevState);
   };
@@ -29,21 +85,23 @@ function Navbar({ inView }: Props) {
     jumpToNav();
     setIsCartActive(() => false);
     setIsMenuActive(() => false);
+    deactivateTabIndexMenu();
     setIsSearchActive(prevState => !prevState);
   };
 
   const removeMenus = () => {
     setIsCartActive(() => false);
     setIsMenuActive(() => false);
+    deactivateTabIndexMenu();
     setIsSearchActive(() => false);
   };
 
-  // Necessary because the menus must be in full height
+  // Necessary, because the menus must be in full height, when opening them.
   const navRef = useRef<HTMLElement>(null);
-
   const jumpToNav = () => {
     if (navRef.current) {
-        navRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        navRef.current.scrollIntoView({ behavior: 'smooth', 
+        block: 'start' });
       }
   }
 
@@ -62,46 +120,47 @@ function Navbar({ inView }: Props) {
   }, [isMenuActive || isCartActive || isSearchActive]);
 
   return (
-    <>
     <nav className="nav" ref={navRef}>
-        <Link className="navLogoContainer" to="/" aria-hidden="true" tabIndex={-1} onClick={removeMenus}>
-                <img className={`navLogo ${!inView && 'active'}`} src="src/assets/veritas-logo-white.svg" alt="veritas" />
+      <div className="navWrapper">
+        <Link className="navLogoContainer" to="/" aria-label='Home-Link' onClick={removeMenus}>
+          <img className={`navLogo ${!inView && 'active'}`} src="src/assets/veritas-logo-white.svg" alt="veritas" />
         </Link>
 
-        <div className={`navLinkContainer ${isMenuActive ? 'active' : ''}`}>
-            <button className="navLink navLinkSearch" onClick={showSearch}>
-                SUCHE
-                <img className="searchIconMenu" src="src/assets/magnify02.svg" alt="" />
-            </button>
-            <NavLink to="/outdoor" className="navLink navLinkOutdoor">OUTDOOR</NavLink>
-            <NavLink to="/workwear" className="navLink navLinkWorkwear">WORKWEAR</NavLink>
-            <NavLink to="/sport" className="navLink navLinkSport">SPORT</NavLink>
-            <NavLink to="/about-us" className="navLink navLinkAboutUs">ÜBER UNS</NavLink>
-        </div>
-
-        <button className="navBtn navBtnBurger" aria-label='Öffnen/Schließen Menü' onClick={showMenu}>
-            <svg
-            className={`ham hamRotate ${isMenuActive ? 'active' : ''}`} viewBox="0 0 100 100" width="33px" height="33px">
-            <path className="line top" d="m 70,33 h -40 c 0,0 -6,1.368796 -6,8.5 0,7.131204 6,8.5013 6,8.5013 l 20,-0.0013" />
-            <path className="line middle" d="m 70,50 h -40" />
-            <path className="line bottom" d="m 69.575405,67.073826 h -40 c -5.592752,0 -6.873604,-9.348582 1.371031,-9.348582 8.244634,0 19.053564,21.797129 19.053564,12.274756 l 0,-40" />
-            </svg>
+        <button className="navBtn navBtnBurger" ref={burgerBtn} aria-label='Öffnen/Schließen Menü' onClick={showCloseMenu}>
+          <svg className={`ham hamRotate ${isMenuActive ? 'active' : ''}`} viewBox="0 0 100 100" width="33px" height="33px">
+          <path className="line top" d="m 70,33 h -40 c 0,0 -6,1.368796 -6,8.5 0,7.131204 6,8.5013 6,8.5013 l 20,-0.0013" />
+          <path className="line middle" d="m 70,50 h -40" />
+          <path className="line bottom" d="m 69.575405,67.073826 h -40 c -5.592752,0 -6.873604,-9.348582 1.371031,-9.348582 
+          8.244634,0 19.053564,21.797129 19.053564,12.274756 l 0,-40" />
+          </svg>
         </button>
+      </div>
 
-        <div className='navBtnContainer'>
-            <button className="navBtn navBtnCart" onClick={showCart}>
-            <img className="navIcon" src="src/assets/cart.svg" alt="cart-button" />
-            </button>
-            <button className="navBtn navBtnSearch" onClick={showSearch}>
-            <img className="navIcon" src="src/assets/magnify.svg" alt="search-button" />
-            </button>
-        </div>
+      <div className={`navLinkContainer ${isMenuActive ? 'active' : ''}`}>
+        <button ref={navLink01} className="navLink navLinkSearch" onClick={showSearch}>
+          SUCHE
+          <img className="searchIconMenu" src="src/assets/magnify02.svg" alt="" />
+        </button>
+        <NavLink ref={navLink02} to="/outdoor" className="navLink navLinkOutdoor" onClick={onClickMenuLink}>OUTDOOR</NavLink>
+        <NavLink ref={navLink03} to="/workwear" className="navLink navLinkWorkwear" onClick={onClickMenuLink}>WORKWEAR</NavLink>
+        <NavLink ref={navLink04} to="/sport" className="navLink navLinkSport" onClick={onClickMenuLink}>SPORT</NavLink>
+        <NavLink ref={navLink05} onKeyDown={(e) => handleBlurLastLink(e)} onClick={onClickMenuLink} to="/about-us" 
+        className="navLink navLinkAboutUs">ÜBER UNS</NavLink>
+      </div>
         
-        <div className={`shoppingCartWindow ${isCartActive ? 'active' : ''}`}></div>
-        <div className={`searchWindow ${isSearchActive ? 'active' : ''}`}></div>
-        <div className={`dark-bg ${isMenuActive || isCartActive || isSearchActive ? 'active' : ''}`} onClick={removeMenus}></div>
+      <div className='navBtnContainer'>
+        <button className="navBtn navBtnCart" onClick={showCart}>
+          <img className="navIcon" src="src/assets/cart.svg" alt="cart-button" />
+          </button>
+        <button className="navBtn navBtnSearch" onClick={showSearch}>
+          <img className="navIcon" src="src/assets/magnify.svg" alt="search-button" />
+        </button>
+      </div>
+          
+      <div className={`shoppingCartWindow ${isCartActive ? 'active' : ''}`}></div>
+      <div className={`searchWindow ${isSearchActive ? 'active' : ''}`}></div>
+      <div className={`dark-bg ${isMenuActive || isCartActive || isSearchActive ? 'active' : ''}`} onClick={removeMenus}></div>
     </nav>
-    </>
   );
 }
 
